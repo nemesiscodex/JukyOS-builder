dnarvaez/olpc-os-builder

sugar_activity_group: add support for cacheonly
To sidestep messing with bitfrost, we cache the parsed
microformat using pickle.

modules/sugar_activity_group/kspost.60.nochroot.activities.py
@@ -10,6 +10,7 @@ import urllib
 import urllib2
 import urlparse
 import time
+import pickle
 
 from bitfrost.update import microformat
 
@@ -35,23 +36,48 @@ if install_activities:
     for suffix in suffixes:
         if len(suffix) > 0:
             grpurl = urlparse.urljoin(baseurl + "/", urllib.quote(suffix))
+            grpurlcache=os.path.join(cache, os.path.basename(baseurl)
+                                            + '-' + suffix + ".html")
         else:
             grpurl = baseurl
+            grpurlcache=os.path.join(cache, os.path.basename(baseurl)
+                                            + ".html")
 
-        print >>sys.stderr, "Trying group URL", grpurl
-        try:
-            name, desc, results = microformat.parse_url(grpurl)
-        except urllib2.HTTPError, e:
-            if e.code == 404:
+        if ooblib.cacheonly:
+            print >>sys.stderr, "Trying group URL cache file", grpurlcache
+            if os.path.exists(grpurlcache):
+                name, desc, results = pickle.load(open(grpurlcache))
+            else:
                 continue
-            raise e
-        if len(results) == 0 or (name is None and desc is None):
-            continue
-        print >>sys.stderr, "Found activity group:", name
+        else:
+            print >>sys.stderr, "Trying group URL", grpurl
+            try:
+                name, desc, results = microformat.parse_url(grpurl)
+            except urllib2.HTTPError, e:
+                if e.code == 404:
+                    continue
+                raise e
+            if len(results) == 0 or (name is None and desc is None):
+                continue
+            print >>sys.stderr, "Found activity group:", name
+            pickle.dump([name, desc, results], file(grpurlcache, 'w'))
 
         for name, info in results.items():
             (version, url) = microformat.only_best_update(info)
             print >>sys.stderr, "Examining %s v%s: %s" % (name, version, url)
+
+            if ooblib.cacheonly:
+                path = urlparse.urlsplit(url)[2]
+                path = os.path.basename(path)
+
+                localpath = os.path.join(cache, path)
+                if os.path.exists(localpath):
+                    print >>sys.stderr, "Using: ", localpath
+                    ooblib.install_sugar_bundle(localpath)
+                    continue
+                else:
+                    print >>sys.stderr, "Cannot find cache for ", url
+                    sys.exit(1)
 
             fd = None
             for attempts in range(5):

nemesiscodex/JukyOS-builder

modules/sugar_activity_group/kspost.60.nochroot.activities.py
@@ -10,6 +10,7 @@ import urllib
 import urllib2
 import urlparse
 import time
+import pickle
 
 from bitfrost.update import microformat
 
@@ -35,23 +36,48 @@ if install_activities:
     for suffix in suffixes:
         if len(suffix) > 0:
             grpurl = urlparse.urljoin(baseurl + "/", urllib.quote(suffix))
+            grpurlcache = os.path.join(cache, os.path.basename(baseurl)
+                                              + '-' + suffix + ".html")
         else:
             grpurl = baseurl
+            grpurlcache = os.path.join(cache, os.path.basename(baseurl)
+                                              + ".html")
 
-        print >>sys.stderr, "Trying group URL", grpurl
-        try:
-            name, desc, results = microformat.parse_url(grpurl)
-        except urllib2.HTTPError, e:
-            if e.code == 404:
+        if ooblib.cacheonly:
+            print >>sys.stderr, "Trying group URL cache file", grpurlcache
+            if os.path.exists(grpurlcache):
+                name, desc, results = pickle.load(open(grpurlcache))
+            else:
                 continue
-            raise e
-        if len(results) == 0 or (name is None and desc is None):
-            continue
-        print >>sys.stderr, "Found activity group:", name
+        else:
+            print >>sys.stderr, "Trying group URL", grpurl
+            try:
+                name, desc, results = microformat.parse_url(grpurl)
+            except urllib2.HTTPError, e:
+                if e.code == 404:
+                    continue
+                raise e
+            if len(results) == 0 or (name is None and desc is None):
+                continue
+            print >>sys.stderr, "Found activity group:", name
+            pickle.dump([name, desc, results], open(grpurlcache, 'w'))
 
         for name, info in results.items():
             (version, url) = microformat.only_best_update(info)
             print >>sys.stderr, "Examining %s v%s: %s" % (name, version, url)
+
+            if ooblib.cacheonly:
+                path = urlparse.urlsplit(url)[2]
+                path = os.path.basename(path)
+
+                localpath = os.path.join(cache, path)
+                if os.path.exists(localpath):
+                    print >>sys.stderr, "Using: ", localpath
+                    ooblib.install_sugar_bundle(localpath)
+                    continue
+                else:
+                    print >>sys.stderr, "Cannot find cache for ", url
+                    sys.exit(1)
 
             fd = None
             for attempts in range(5):