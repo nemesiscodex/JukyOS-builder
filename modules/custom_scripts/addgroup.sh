3bb6b0f12dff627d07827a604f8abbff60e55758
Julio Reyes <nemesiscodex@gmail.com>
2013-05-08 10:40:03 -0400

    Agregado script para actualizacion de Actividades de ParaguayEduca

---
+++ modules/custom_scripts/addgroup.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+#Este script agrega la url del servidor de ParaguayEduca para actualizar las actividades 
+cat "http://activities.paraguayeduca.org/Juky/1.0/" > /home/olpc/Activities/.groups
