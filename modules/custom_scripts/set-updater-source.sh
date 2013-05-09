a75780d74ecf30b21396fafd5b3785d799ca7434
Julio Reyes <nemesiscodex@gmail.com>
2013-05-09 08:44:37 -0400

    agregado directorio faltante al path en set-updater-source.sh

--- modules/custom_scripts/set-updater-source.sh
+++ modules/custom_scripts/set-updater-source.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #Este script agrega la url del servidor de ParaguayEduca para actualizar las actividades 
-cp $PWD/misc/juky-updater-source-1.0 $INSTALL_ROOT/home/olpc/.groups
+cp $PWD/misc/juky-updater-source-1.0 $INSTALL_ROOT/home/olpc/Activities/.groups
