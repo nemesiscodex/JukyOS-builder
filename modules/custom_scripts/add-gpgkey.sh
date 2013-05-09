7fcba13e7a3c27cff753aa23c91c26377a9d595f
Julio Reyes <nemesiscodex@gmail.com>
2013-05-09 09:54:10 -0400

    add-gpgkey.sh Script para agregar la clave publica de los repositorios de paraguayeduca

---
+++ modules/custom_scripts/add-gpgkey.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cp $PWD/misc/RPM-GPG-KEY-paraguayeduca.org $INSTALL_ROOT/tmp/
+/usr/sbin/chroot $INSTALL_ROOT rpm --import /tmp/RPM-GPG-KEY-paraguayeduca.org
+rm $INSTALL_ROOT/tmp/RPM-GPG-KEY-paraguayeduca.org
