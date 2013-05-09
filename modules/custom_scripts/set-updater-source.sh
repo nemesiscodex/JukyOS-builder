3b0a56debb3aacbc126de9273ecb4369b7c2996a
Julio Reyes <nemesiscodex@gmail.com>
2013-05-09 09:58:59 -0400

    Agregado mensajes a los custom scripts

--- modules/custom_scripts/set-updater-source.sh
+++ modules/custom_scripts/set-updater-source.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 #Este script agrega la url del servidor de ParaguayEduca para actualizar las actividades 
+echo "Agregando URL de actividades de ParaguayEduca"
 cp $PWD/misc/juky-updater-source-1.0 $INSTALL_ROOT/home/olpc/Activities/.groups
