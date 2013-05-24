46231278c0171333842adb6076518e38637fd257
Julio Reyes <nemesiscodex@gmail.com>
2013-05-24 10:32:55 -0400

    Cambiado versiones de actividades 	Agregado un custom script para cambiar el logo de fedora por el de la FPUNA

---
+++ modules/custom_scripts/bootimg-replace.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+#Este script agrega la url del servidor de ParaguayEduca para actualizar las actividades 
+echo "Reemplazando fedora bootimg"
+cp $PWD/bootimg/FPUNA.png $INSTALL_ROOT/usr/share/plymouth/themes/olpc/fedora.png
